bdd57e5b29efcdb539751b7fdd45be7defe4129a
MathiasDWaterlaw <mathiasdellosa@gmail.com>
2024-04-22 22:50:34 +0200

    added working js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,31 @@
 const adviceId = document.getElementById('advice-id');
 const quote = document.getElementById('quote');
+const btn = document.getElementById('dice-btn');
+
+const apiUrl = 'https://api.adviceslip.com/advice';
+
+async function searchAdvice() {
+    
+    try{
+        const result = await fetch(apiUrl)
+        .then((data) => data.json());
+
+        showAdvice(result.slip);
+
+    } catch (error) {
+        console.error(error);
+        alert('Advice not found');
+    }
+}
+
+const showAdvice = (slip) => {
+    const { id , advice } = slip;
+    console.log(id);
+    console.log(advice);
+    adviceId.innerText = `${id}`;
+    quote.innerText = `"${advice}"`;
+}
+
+searchAdvice();
+
+btn.addEventListener('click', searchAdvice);
